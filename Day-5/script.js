29b6663a0f739764ba38da54e2c178f554c5c3e9
Himanshu Chauhan <159509944+Himanshu-ABES@users.noreply.github.com>
2024-10-14 10:25:52 +0530

    Day-5 Uploaded

---
+++ Day-5/script.js
@@ -0,0 +1,36 @@
+// function display(){
+//     alert("hello Js")
+// }
+function display(){
+    console.log("hello Js")
+}
+
+let x = 12.23;
+console.log(typeof(x));
+
+let y = "hello";
+console.log(typeof(y));
+
+let z = true;
+console.log(typeof(z));
+
+let obj = {
+    name: "Rahul",
+    age: 19
+}
+console.log(typeof(obj));
+
+let arr = [1,2,3,4,5];
+console.log(typeof(arr));
+
+// let a;
+// console.log(a);
+
+let b = null;
+console.log(typeof(b));
+
+let a = ()=>{
+    console.log("hello");
+}
+
+a()
